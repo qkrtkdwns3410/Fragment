a7115d0d2dea5a9d6533c6a906a1418d7caeb10e
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-03-28 02:58:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fragmentkt/Fragment2.kt
@@ -0,0 +1,20 @@
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.example.fragmentkt.R
+
+class Fragment2 : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.frag2, container,false)
+
+
+
+
+
+        return view
+    }
+}
